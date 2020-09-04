0b2c4d4c57da026b42d4d79a64307014abd2889c
Thomas Guyader <thomas.guyader@gouv.nc>
2020-09-04 16:11:10 +1100

    feat(android): check des permissions au lancement

--- mycamerai/main.py
+++ mycamerai/main.py
@@ -1,5 +1,7 @@
 import logging
 
+from android.permissions import request_permissions, Permission
+from kivy import platform
 from kivymd.app import MDApp
 from kivy.logger import Logger
 from kivy.properties import ObjectProperty
@@ -17,6 +19,30 @@ class PhotoApp(MDApp):
 
     config = None
     face_detector = None
+
+    def request_android_permissions(self):
+        """
+        Since API 23, Android requires permission to be requested at runtime.
+        This function requests permission and handles the response via a
+        callback.
+        The request will produce a popup if permissions have not already been
+        been granted, otherwise it will do nothing.
+        """
+
+        def callback(permissions, results):
+            """
+            Defines the callback to be fired when runtime permission
+            has been granted or denied. This is not strictly required,
+            but added for the sake of completeness.
+            """
+            if all([res for res in results]):
+                print("callback. All permissions granted.")
+            else:
+                print("callback. Some permissions refused.")
+
+        request_permissions([Permission.CAMERA,
+                             Permission.READ_EXTERNAL_STORAGE,
+                             Permission.WRITE_EXTERNAL_STORAGE], callback)
 
     def build_config(self, config):
         config.setdefaults("Label", {"Content": "Default label text"})
@@ -39,6 +65,9 @@ class PhotoApp(MDApp):
         settings.add_json_panel("Configuration", self.config, data=jsondata)
 
     def build(self):
+        if platform == "android":
+            print("Android detected. Requesting permissions")
+            self.request_android_permissions()
         self.manager = AppScreenManager()
         self.manager.add_widget(Capture(name="Capture"))
         self.manager.add_widget(Send(name="Send"))
